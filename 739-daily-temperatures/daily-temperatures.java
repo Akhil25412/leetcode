c248c6a501b75046bc80ba782f9a3276e082a366
Akhil25412 <vtu25412@veltech.edu.in>
2026-02-08 18:22:47 +0530

    Time: 61 ms (67.61%) | Memory: 107.9 MB (13.74%) - LeetSync

---
+++ 739-daily-temperatures/daily-temperatures.java
@@ -0,0 +1,20 @@
+import java.util.Stack;
+
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int[] answer = new int[n];
+        Stack<Integer> stack = new Stack<>(); // stores indices
+
+        for (int i = 0; i < n; i++) {
+            // Pop indices with lower temperature than current
+            while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+                int prevIndex = stack.pop();
+                answer[prevIndex] = i - prevIndex;
+            }
+            stack.push(i);
+        }
+
+        return answer;
+    }
+}
