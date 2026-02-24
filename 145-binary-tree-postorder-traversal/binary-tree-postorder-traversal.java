590fe3aaf982201513b3dc1b580377cae95ee4a6
Akhil25412 <vtu25412@veltech.edu.in>
2026-02-24 10:01:32 +0530

    Time: 1 ms (13.90%) | Memory: 43.5 MB (13.88%) - LeetSync

---
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Solution {
+
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+
+        while (!stack.isEmpty()) {
+            TreeNode current = stack.pop();
+            result.add(0, current.val); // Add at beginning
+
+            if (current.left != null) {
+                stack.push(current.left);
+            }
+            if (current.right != null) {
+                stack.push(current.right);
+            }
+        }
+
+        return result;
+    }
+}
